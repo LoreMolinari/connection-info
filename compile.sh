15b2c2f1b34efe2bad9dd9920dcf057e93a0423e
LoreMolinari <lore.molinari.00@gmail.com>
2022-08-27 09:15:37 +0200

    Fixed WebServer
    Fixed WebServer

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 gcc -o server server.c;
 gcc -o client client.c;
+gcc -o webserver webserver.c;
 
 ls -lah;
--- webserver.c
+++ webserver.c
@@ -9,107 +9,115 @@
 #include <signal.h>
 #include "ipaddress.h"
 
-void send_response(FILE *f, char *key, char *value)
-{
-  fprintf(f, "HTTP/1.1 200 OK\r\n");
-  fprintf(f, "Content-Type: application/json\r\n");
-  fprintf(f, "\r\n");
-  fprintf(f, "{\"%s\": \"%s\"}", key, value);
-}
-
-int open_connection(int port)
-{
-  int sock;
-  struct sockaddr_in addr_in;
-
-  printf("simpleserver::Creating socket: (%d, %d, 0)\n", AF_INET, SOCK_STREAM);
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-  printf("simpleserver::Socket created (%d)\n", sock);
-
-  if (sock == -1)
-  {
-    printf("Failed to create socket (%d)\n", errno);
-    exit(EXIT_FAILURE);
-  }
-
-  addr_in.sin_family = AF_INET;
-  addr_in.sin_port = htons(port);
-  addr_in.sin_addr.s_addr = INADDR_ANY;
-
-  printf("simpleserver::bind (%d, (%d, %d, %d), %lu)\n", sock, addr_in.sin_family, addr_in.sin_port, addr_in.sin_addr.s_addr, sizeof(struct sockaddr_in));
-
-  bind(sock, (struct sockaddr *)&addr_in, sizeof(struct sockaddr_in));
-
-  printf("simpleserver::listen(%d, 8)\n", sock);
-
-  if (listen(sock, 8) == -1)
-  {
-    printf("Failed to get socket to listen (%d)\n", errno);
-    exit(EXIT_FAILURE);
-  }
-
-  return sock;
-}
-
-void accept_client(int sock, char *key)
-{
-  struct sockaddr_in client_addr;
-  socklen_t clientaddr_len;
-  FILE *f;
-
-  printf("simpleserver::accept(%d)\n", sock);
-
-  int client_sock = accept(sock, (struct sockaddr *)&client_addr, &clientaddr_len);
-  printf("simpleserver::accepted (%d)\n", client_sock);
-  if (client_sock == -1)
-  {
-    printf("Failed to accept connection (%d)\n", errno);
-    exit(EXIT_FAILURE);
-  }
-
-  printf("simpleserver::fdopen(%d)\n", client_sock);
-  f = fdopen(client_sock, "w+");
-  if (f == NULL)
-    printf("simpleserver::Error during fdopen\n");
-
-  printf("simpleserver::send_response(%s)\n", key);
-  send_response(f, key, get_ip_address(f));
-
-  printf("simpleserver::fclose()\n");
-  fclose(f);
-
-  printf("simpleserver::fflush\n");
-  // stdout needs to be flushed in order for heroku to read the logs
-  fflush(stdout);
-  printf("simpleserver::endfflush\n");
-}
-
-/**
- * This HTTP server can only send HTTP responses.
- * It requires header files such as ipaddress.h to read the headers.
- */
-void send_only(int port, char *key)
-{
-  int sock = open_connection(port);
-
-  while (1)
-  {
-    accept_client(sock, key);
-  }
-  close(sock);
-}
+#define PORT 9090
+#define LISTEN 5
+#define MAXLEN 255
 
 int main(int argc, char **argv)
 {
-  // if (argc != 2)
-  // {
-  //   printf("%s [port-number]\n", argv[0]);
-  //   exit(EXIT_FAILURE);
-  // }
 
-  // int port = atoi(argv[1]);
-  int port = 9090;
-  send_only(port, "localhost");
+  struct sockaddr_in bind_ip_port;
+  int bind_ip_port_length = sizeof(bind_ip_port);
 
-  exit(EXIT_SUCCESS);
+  int serverdescriptor = socket(AF_INET, SOCK_STREAM, 0);
+  if (serverdescriptor < 0) die("socket() error.\n");
+  printf("socket() ok.\n");
+
+  bind_ip_port.sin_family = AF_INET; 
+  bind_ip_port.sin_addr.s_addr = inet_addr("127.0.0.1");
+  bind_ip_port.sin_port = htons(PORT);
+
+  if(bind(serverdescriptor, (struct sockaddr *) &bind_ip_port, bind_ip_port_length) < 0) die("bind() error.\n");
+  printf("bind() ok.\n");
+
+  if(listen(serverdescriptor, LISTEN) < 0) die("listen() error.\n");
+  printf("listen ok.\n");
+
+  while (1){
+
+    struct sockaddr_in client_addr;
+    socklen_t clientaddr_len;
+
+    int clientdescriptor = accept(serverdescriptor, (struct sockaddr *)&client_addr, &clientaddr_len);
+    if (clientdescriptor < 0) die("accept() error.\n");
+    printf("accept() ok.\n");
+    printf("---------- new client connected ----------\n");
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+      die("fork() error.\n"); 
+    } else if (pid > 0) {
+      close(clientdescriptor); 
+    } else if (pid == 0) {
+      close(serverdescriptor);
+      handleClient(clientdescriptor);
+      printf("---------- client disconnected ----------\n");
+      close(clientdescriptor);
+      exit(0);
+    }
+
+  }
+
+  close(serverdescriptor);
+  return 0;
+}
+
+
+void handleClient(int clientdescriptor) {
+
+    FILE* f;
+
+    f = fdopen(clientdescriptor, "w+");
+    if (f == NULL) die("fdopen() error.\n");
+    printf("fdopen() ok.\n");
+
+    sendFirstResponse(f, get_ip_address(f), "test");
+
+    fclose(f);
+    // stdout needs to be flushed in order for heroku to read the logs
+    fflush(stdout);
+    printf("fflush()\n");
+
+}
+
+void sendFirstResponse(FILE *f, char *key, char *value)
+{
+
+  const char htmlResponse[] = "\r\n <html>\r\n <head>\r\n <title>Protocols calculations</title>\r\n </head>\r\n <body>\r\n"
+  "<h1>Scegliere il metodo da eseguire: </h1>\r\n <form action='' method='get'> <input type='submit' id='t' name='choice' value='Throughput'>\r\n"
+  "<input type='submit' id='i' name='choice' value='IdleRQ'>\r\n <input type='submit' id='w' name='choice' value='AdvertisedWindow'>\r\n"
+  "<input type='submit' id='TT' name='choice' value='Timeout'></form>\r\n"
+  "</body>\r\n </html>\r\n\r\n";
+
+  fprintf(f, "HTTP/1.1 200 OK\r\n");
+  fprintf(f, "Content-Type: text/html\r\n");
+  fprintf(f, htmlResponse);
+}
+
+/*void methodSelection(clientdescriptor){
+
+  char* selection;
+  selection = get_selection();
+  
+  if (strcmp(selection, "T") == 0) {
+    throughput(clientdescriptor);
+  } else if (strcmp(selection, "I") == 0) {
+    idleRQ(clientdescriptor);
+  } else if (strcmp(selection, "W") == 0) {
+    window(clientdescriptor);
+  } else if (strcmp(selection, "TT") == 0) {
+    timeout(clientdescriptor);
+  } else {
+    invalidmethod();
+  }
+
+}*/
+
+//funzione usata in caso di erroe per terminare il programma e stampare un messaggio di errore
+void die(char *error) {
+    
+    fprintf(stderr, "%s.\n", error);
+    exit(1);
+
 }
